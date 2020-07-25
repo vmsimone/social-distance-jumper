9406d97dfdcf23d55ec576f399824ccf011d5a80
Vincent Simone <vsimone@protonmail.com>
2020-07-25 09:11:19 -0700

    now accounts for device pixel ratio

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ import { gameScene } from "./scenes/game.js";
 import { pausedScene } from "./scenes/paused.js";
 import { gameOverScene } from "./scenes/gameover.js";
 
+console.log(window.devicePixelRatio);
+console.log(screenBuffer(window.innerHeight));
+
 let game = new Phaser.Game({
   type: Phaser.AUTO,
   //the game should always be vertical or square
@@ -37,6 +40,7 @@ let game = new Phaser.Game({
   ],
   pixelArt: true
 });
+
 
 function returnLowest(a, b) {
   if (a < b) {
